da538ad4f038d80b799a8ef0ab824fae1ec5badf
Tim Copland <tim.copland@august.com.au>
2022-04-16 09:29:28 +1000

    Remove old font

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,6 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet" />
-        </Head>
         <body className="bg-gunpowder">
           <Main />
           <NextScript />
